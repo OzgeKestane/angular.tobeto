e5b1433a9d56b255e18029d94b2c269631b553be
Özge Kestane <ozgekestane97@gmail.com>
2024-03-11 14:58:39 +0300

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-//annotation, attribute, decorator
+// annotation, attribute, decorator
 @Component({
-  selector: 'app-root', //<approot> </approot>
-  standalone: true, //sonradan bakılacak
-  imports: [RouterOutlet,FormComponent],
+  selector: 'app-root', // <app-root> <app-root/>
+  standalone: true, // sonradan bakılacak.
+  imports: [RouterOutlet, FormComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tobeto';
-  count=0;
-  inputValue:string='';
-
-  onBtnClick(event:Event){
-    //console.log("tıklandı");
-    //console.log(event);
+  title = 'Tobeto';
+  count = 0;
+  inputValue: string = '';
+  onBtnClick(event: Event) {
     console.log(this.inputValue);
-
     this.count++;
-
   }
-  onChange(event:Event){
-    let element=event.target as HTMLInputElement;
-    console.log("input değeri değişti",element.value);
-    this.inputValue=element.value;
+  onChange(event: Event) {
+    let element = event.target as HTMLInputElement;
+    console.log('Input değeri değişti', element.value);
+    this.inputValue = element.value;
   }
 }
