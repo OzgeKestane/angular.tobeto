219c1fbfc827afa33a1da245d0afa353e91eca9c
Özge Kestane <ozgekestane97@gmail.com>
2024-03-11 15:09:04 +0300

    ngfor & ngif

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  //selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrl: './homepage.component.scss',
 })
 export class HomepageComponent {
-
+  todoList: string[] = ['Eleman 1'];
 }
