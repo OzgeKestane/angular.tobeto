1b39c4282267a0556201c36b63a6a46eeb08e0bf
Özge Kestane <ozgekestane97@gmail.com>
2024-03-13 14:05:06 +0300

    myinput component

--- src/app/components/base-input/base-input.component.spec.ts
+++ src/app/components/base-input/base-input.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarComponent } from './navbar.component';
+import { BaseInputComponent } from './base-input.component';
 
-describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+describe('BaseInputComponent', () => {
+  let component: BaseInputComponent;
+  let fixture: ComponentFixture<BaseInputComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
+      imports: [BaseInputComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(NavbarComponent);
+    fixture = TestBed.createComponent(BaseInputComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
